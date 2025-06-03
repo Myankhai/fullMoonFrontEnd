09fcd66e4ca0fa07728edd0af088d85415ec557d
Myankhai <Myankhai@users.noreply.github.com>
2025-06-03 08:46:39 +0000

    deploy: 343433ea5422e052dab73295a8430c0747df61a1

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { UserConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/fullMoonFrontEnd/',  // Replace with your repository name
-} as UserConfig) 
